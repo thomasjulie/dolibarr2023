7d97230a3bb22dd6fe906e451ec848eba60786f0
root <root@dolibarr17.fr>
2023-08-21 15:51:03 +0200

    creation object famille avec ces pages

---
+++ htdocs/custom/creche/sql/llx_creche_enfants.sql
@@ -0,0 +1,34 @@
+-- Copyright (C) 2023 SuperAdmin
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see https://www.gnu.org/licenses/.
+
+
+CREATE TABLE llx_creche_enfants(
+	-- BEGIN MODULEBUILDER FIELDS
+	rowid int AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+	fk_famille int NOT NULL, 
+	genre varchar(50) NOT NULL, 
+	nom varchar(150) NOT NULL, 
+	prenom varchar(150) NOT NULL, 
+	date_naissance date NOT NULL, 
+	vaccination integer NOT NULL, 
+	droit_image integer NOT NULL, 
+	infos_divers text NOT NULL, 
+	photo_id varchar(250) NOT NULL, 
+	fk_user_create integer NOT NULL, 
+	date_create datetime NOT NULL, 
+	fk_user_update integer NOT NULL, 
+	date_update datetime NOT NULL
+	-- END MODULEBUILDER FIELDS
+) ENGINE=innodb;
---
+++ htdocs/custom/creche/sql/llx_creche_parents.sql
@@ -0,0 +1,38 @@
+-- Copyright (C) 2023 SuperAdmin
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see https://www.gnu.org/licenses/.
+
+
+CREATE TABLE llx_creche_parents(
+	-- BEGIN MODULEBUILDER FIELDS
+	rowid int AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+	fk_famille int NOT NULL, 
+	civilite varchar(10) NOT NULL, 
+	nom varchar(150) NOT NULL, 
+	prenom varchar(150) NOT NULL, 
+	adresse varchar(250) NOT NULL, 
+	code_postal varchar(5) NOT NULL, 
+	ville varchar(250) NOT NULL, 
+	tel_portable varchar(20) NOT NULL, 
+	mail varchar(150) NOT NULL, 
+	login varchar(100) NOT NULL, 
+	mdp varchar(150) NOT NULL, 
+	notif integer NOT NULL, 
+	mode_notif varchar(150) NOT NULL, 
+	fk_user_create integer NOT NULL, 
+	date_create datetime NOT NULL, 
+	fk_user_update integer NOT NULL, 
+	date_update datetime NOT NULL
+	-- END MODULEBUILDER FIELDS
+) ENGINE=innodb;
